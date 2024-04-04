f50188e8dd4b80409f8dd6c0c8cf90211d340686
Mitesh Meghani <miteshmeghani310@gmail.com>
2024-04-04 09:13:50 +0530

    webdevlopment page ,getin touch create componants

--- src/Componants/service/WebDevelopment.jsx
+++ src/Componants/service/WebDevelopment.jsx
@@ -1,29 +1,15 @@
 import React from 'react'
 import Banner from './Banner'
 import './webdevlopment.css'
-import Header from '../header/Header'
-import Footer from '../footer/Footer'
+import Getin from './Getin'
+
 
 export default function WebDevelopment() {
   return (
     <>
-    <Header/>
-      <Banner />
-        {/* <div>
-        <marquee
-          behavior="scroll"
-          direction="up"
-          scrollamount={8}
-          style={{ width: "100%" }}
-        >
-          <img className='ma-img' src="./image/webdevlopment/Untitled-12.jpg" alt="Image 1" />
-          <img className='ma-img' src="./image/webdevlopment/Untitled-13.jpg" alt="Image 2" />
-          <img className='ma-img' src="./image/webdevlopment/Untitled-14.jpg" alt="Image 3" />
-          <img className='ma-img' src="./image/webdevlopment/Untitled-15.jpg" alt="Image 4" />
-          <img className='ma-img' src="./image/webdevlopment/Untitled-16.jpg" alt="Image 5" />
-        </marquee>
 
-        </div> */}
+      <Banner />
+       
       <div className="container">
         <div className="web">
           <h2>Comprehensive Web Design and Development Services</h2>
@@ -149,62 +135,128 @@ export default function WebDevelopment() {
           </div>
         </div>
       </div> 
-      <div className="get-touch">
+      <Getin/>
+      
+      <div className="data">
         <div className="container">
-          <h2 className='py-5'>Get In Touch With Best Software <br /> Consulting Company</h2>
-          <div className="row py-5">
-            <div className="col-lg-4 mb-3">
-              <input
-                type="name"
-                class="form-control"
-                id="exampleFormControlInput1"
-                placeholder="Your Name"
-                required
-              />
-            </div>
-            <div className="col-lg-4 mb-3">
-              <input
-                type="email"
-                class="form-control"
-                id="exampleFormControlInput1"
-                placeholder="Email Address"
-                required
-              />
-            </div>
-            <div className="col-lg-4 mb-3">
-              <input
-                type="name"
-                class="form-control"
-                id="exampleFormControlInput1"
-                placeholder="Phone Number"
-                required
-              />
-            </div>
-            <div class="mb-3">
-                <textarea
-                  class="form-control"
-                  id="exampleFormControlTextarea1"
-                  placeholder="Message"
-                  rows="3"
-                  required
-                ></textarea>
+          <h2 className='mt-5'>Our Detailed Data Analytics Process</h2>
+          <div className="row mt-5">
+            <div className="col-lg-4">
+              <div className="chek mt-5">
+                <span className='one'>01</span>
+                <h1 className='mb-4'> Planning and Discovery</h1>
               </div>
-              <center><button>INQUIRE NOW</button></center>
-              <div className="col-4"></div>
-              <div className="col-4">
-                <div class="card">
-                  <div class="card-body">
-                    <h5 class="card-title"><i class="fa-regular fa-envelope"></i> Info@Weblockinfosoft.com</h5><hr />
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    
-                  </div>
+            </div>
+            <div className="col-lg-4">
+              <center><img src="./image/webdevlopment/Mask group (4).png" alt="" /></center>
+            </div>
+            <div className="col-lg-4 mt-5">
+              <p>Define Goals: Understand the purpose of the website or web application, target audience, and desired outcomes.Research: Conduct market research, competitor analysis, and gather requirements from stakeholders.</p>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-lg-4  mt-5">
+              <p>Wireframing: Create wireframes or low-fidelity prototypes to outline the structure and layout of key pages and features.
+                UI/UX Design: Develop high-fidelity designs focusing on user interface (UI) design elements such as colors, typography, icons, and user experience (UX) considerations like navigation, interactions, and usability.</p>
+            </div>
+            <div className="col-lg-4">
+              <center><img src="./image/webdevlopment/Mask.png" alt="" /></center>
+            </div>
+            <div className="col-lg-4 mt-5">
+              <div className="chek mt-5">
+                  <h1> Design Phase</h1>
+                  <span className='two'>02</span>
+                </div>
+              </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-lg-4">
+              <div className="chek mt-5">
+                <span className='three'>03</span>
+                <h1> Development</h1>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <center><img src="./image/webdevlopment/Mask group (3).png" alt="" /></center>
+            </div>
+            <div className="col-lg-4 mt-5">
+              <p>Front-end Development brings UI/UX designs to life using HTML, CSS, JavaScript, and frameworks like React, Angular, or Vue.js. Back-end Development manages server-side logic, databases, and functionality with languages like Python, PHP, Ruby, Java, or Node.js, utilizing frameworks such as Django, Laravel, Ruby on Rails, Spring Boot, or Express.js based on project requirements.</p>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-lg-4 mt-5">
+              <p>Functional Testing verifies features work as intended, Compatibility Testing ensures performance across devices, Usability Testing gathers feedback for user experience improvements, and Security Testing safeguards data and authentication.</p>
+            </div>
+            <div className="col-lg-4">
+              <center><img src="./image/webdevlopment/image 90 (1).png" alt="" /></center>
+            </div>
+            <div className="col-lg-4 mt-5">
+              <div className="chek mt-5">
+                  <h1> Testing</h1>
+                  <span className='four'>04</span>
+                </div>
+              </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-lg-4">
+              <div className="chek mt-5">
+                <span className='five'>05</span>
+                <h1> Deployment</h1>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <center><img src="./image/webdevlopment/image 90.png" alt="" /></center>
+            </div>
+            <div className="col-lg-4 mt-5">
+              <p>Server Setup involves choosing a hosting environment (shared, VPS, cloud), configuring servers, databases, domains, and SSL certificates. Deploy Code includes uploading web application files, configuring settings, and ensuring functionality in the production environment. Monitor Performance using tools to track performance metrics, uptime, response times, and server resources for optimization.</p>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-lg-4  mt-5">
+              <p>Regular updates are crucial for website security and performance, ensuring software and plugins are up-to-date to prevent vulnerabilities. Continuous content updates maintain user engagement and relevance, meeting evolving business needs. Consistent technical support and bug fixes ensure smooth functionality, enhancing user satisfaction and operational efficiency.</p>
+            </div>
+            <div className="col-lg-4">
+              <center><img src="./image/webdevlopment/Mask group (2).png" alt="" /></center>
+            </div>
+            <div className="col-lg-4 mt-5">
+              <div className="chek mt-5">
+                  <h1> Maintenance and Updates</h1>
+                  <span className='six'>06</span>
+                </div>
+              </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-lg-4">
+              <div className="chek mt-5">
+                <span className='seven'>07</span>
+                <h1 className='mb-4'> Optimization and SEO</h1>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <center><img src="./image/webdevlopment/Mask group (1).png" alt="" /></center>
+            </div>
+            <div className="col-lg-4 mt-5">
+              <p>To enhance website performance, focus on improving speed through code optimization, image compression, and caching techniques. Additionally, implement SEO strategies such as optimizing meta tags, headings, content, and site structure, along with creating sitemaps, robots.txt files, and schema markup, to boost indexing and ranking on search engines.</p>
+            </div>
+          </div>
+          <div className="row my-5">
+            <div className="col-lg-4 mt-5">
+              <p>Gathering feedback from users, stakeholders, and analytics data allows for informed decision-making in implementing iterative improvements, introducing new features, and making enhancements based on feedback and performance metrics, ensuring continuous iteration and improvement of the website/application to align with evolving business goals and user needs.</p>
+            </div>
+            <div className="col-lg-4">
+              <center><img src="./image/webdevlopment/Mask group.png" alt="" /></center>
+            </div>
+            <div className="col-lg-4 mt-5">
+              <div className="chek mt-5">
+                  <h1> Feedback and Iteration</h1>
+                  <span className='eight'>08</span>
                 </div>
               </div>
           </div>
         </div>
       </div>
 
-      <Footer />
+
     </>
   )
 }
